078bcae8207ae6cb97c41440a6d9d15cfeb3c2e5
liang3030 <liangzhang3030@gmail.com>
2022-10-01 13:08:45 +0200

    implement deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,23 @@
-import { ethers } from "ethers";
+import { BigNumber, ethers } from "ethers";
 import * as dotenv from "dotenv";
+import { Ballot__factory } from "../typechain-types";
 
 dotenv.config()
-
-
 
 const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
 const bytes32Strings = PROPOSALS.map(proposal => ethers.utils.formatBytes32String(proposal));
 
 const main = async () => { 
   console.info('start to deploy contract');
-  
+  const provider = ethers.getDefaultProvider('goerli');
 
+  const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC?? "")
+  const signer = wallet.connect(provider);
 
+  const BallotFactory = new Ballot__factory(signer);
+  const ballotContract = await BallotFactory.deploy(bytes32Strings);
+  await ballotContract.deployed()
+  console.info(`Ballot contract was deployed to the address ${ballotContract.address}`)
 }
 
 main().catch(error => { 
